5c7c25560586bc678e84f1d845777a3feed8467d
alamgirweb11 <alamgirweb11@gmail.com>
2021-03-24 01:22:54 +0600

    constant

---
+++ constant.js
@@ -0,0 +1,10 @@
+// js constant
+const PI = 3.14159265359;
+console.log(PI);
+const product = {category:"men",color:"white",product_name:"pant"};
+console.log(product.category);
+const constArray = ["Alim","John","Richi"];
+constArray[0] = "Aziz";
+console.log(constArray);
+// print specific one
+console.log(constArray[2]); // Richi
