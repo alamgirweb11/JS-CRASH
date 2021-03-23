4b49bbbc570739e52c5d46e339f804628647d48e
alamgirweb11 <alamgirweb11@gmail.com>
2021-03-24 00:48:09 +0600

    variable declaration

---
+++ variable.js
@@ -0,0 +1,19 @@
+let country = 'Bangladesh';
+console.log(country);
+let a_2 = 'variable declartion with string and number';
+console.log(a_2);
+let x = 2;
+let y = 5;
+let z = x+y+"5"; //count 5 as a string value not number
+console.log(z);
+var $_a = 5;
+var $_b = 5;
+var result = $_a+$_b;
+console.log(result);
+//concatenate value
+let userName = "Abir"+" "+"Ahmed";
+console.log(userName); 
+let _first = 'one';
+let _2nd = 'Second';
+let both2 = _first + " " + _2nd;
+console.log(both2);
