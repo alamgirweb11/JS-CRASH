23932b4616106b5600fb18778f1146c7d31818f5
alamgirweb11 <alamgirweb11@gmail.com>
2021-03-26 01:28:26 +0600

    basic discussion of operator

---
+++ operators.js
@@ -0,0 +1,32 @@
+// basic discussion of js operator
+/** Arithmatic operator
+ * // +,-,/,*,++,--, % Modulus (Remainder)
+ */
+// use of arithmatic operator
+x = 13;
+y = 9;
+let z = x+y;
+let xy = z - y;
+let yz = xy * y;
+console.log(z);
+console.log(xy);
+console.log(yz);
+console.log(x++);
+console.log(x++);
+console.log(y--);
+console.log(y--);
+let zx = yz % 7;
+console.log(zx);
+
+/**
+ * Assignment Operator
+ * // =,+=,-=,*=,/=,%=
+ */
+
+// use of assignment operator
+ let a  = 20;
+ console.log( a += 5);
+ console.log( a -= 5);
+ console.log( a *= 5);
+ console.log( a /= 5);
+ console.log( a %= 5);
