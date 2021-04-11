babe0faa1a8c55103a16a5a5ba75210cd68191a1
alamgirweb11 <alamgirweb11@gmail.com>
2021-04-12 02:30:19 +0600

    function declaration

---
+++ function.js
@@ -0,0 +1,18 @@
+// functin declaration
+function math(){
+      console.log(5+5);
+}
+math();
+
+// pass parameter
+function param(name,email){
+    console.log(name);
+    console.log(email);
+}
+param("Demo","demo@gmail.com");
+
+ function add(a,b){
+    return a*b;
+}
+let addition = add(4,4)*3;
+console.log(addition);
