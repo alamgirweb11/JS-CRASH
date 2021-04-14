469fc725f8495753434b9a4248e6d8bc1b3e018d
alamgirweb11 <alamgirweb11@gmail.com>
2021-04-14 23:58:43 +0600

    basic array

---
+++ array.js
@@ -0,0 +1,20 @@
+// basic discussion about array
+let names = ['Abir','Akash','Ashik'];
+console.log(names[0])
+
+// array traversing
+let info = ['Abir','abir@gmail.com','0140000000'];
+for(i=0; i<info.length; i++){
+    console.log(info[i]);
+}
+// total length check
+console.log(info.length);
+// last item
+console.log(info[info.length-1]);
+// index position against value
+console.log(info.indexOf('abir@gmail.com'));
+// add value in last using push method
+let addValue = info.push("push method");
+// array sort using sort method
+let arraySort = info.sort();
+console.log(arraySort);
