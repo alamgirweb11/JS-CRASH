ea60aefb16b796ed67a5126e71f19b15189f944d
alamgirweb11 <alamgirweb11@gmail.com>
2021-04-14 23:36:21 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,11 @@
+// for loop
+let language = "javascript";
+for (let i = 0; i <= 10; i++) {
+      console.log("Language name is " + language);
+}
+// while loop
+let i = 1;
+while(i < 10){
+        i++;
+      console.log(i);
+}
